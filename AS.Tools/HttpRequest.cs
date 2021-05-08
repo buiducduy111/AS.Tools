a63d4038b4e156bb52005f082189a1a9a8709579
Duy Bui <buiducduy111@gmail.com>
2021-05-08 14:21:23 +0700

    Update logger

---
+++ AS.Tools/HttpRequest.cs
@@ -0,0 +1,185 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS.Tools
+{
+    /// <summary>
+    /// Http request helper
+    /// </summary>
+    public class HttpRequest
+    {
+        /// <summary>
+        /// User agent
+        /// </summary>
+        public string UserAgent { get; set; }
+
+        /// <summary>
+        /// Accept
+        /// </summary>
+        public string Accept { get; set; }
+
+        /// <summary>
+        /// Accept Language
+        /// </summary>
+        public string AcceptLanguage { get; set; }
+
+        /// <summary>
+        /// Security type
+        /// </summary>
+        public SecurityProtocolType SecurityProtocolType { get; set; }
+
+        /// <summary>
+        /// Init
+        /// </summary>
+        public HttpRequest()
+        {
+            this.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36";
+            this.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+            this.AcceptLanguage = "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5,ja;q=0.4";
+            this.SecurityProtocolType = SecurityProtocolType.Tls12;
+        }
+
+        /// <summary>
+        /// Get string from a http request. Return html or REQUEST_ERROR_
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="authUser"></param>
+        /// <param name="authPass"></param>
+        /// <param name="useSSL"></param>
+        /// <returns></returns>
+        public string GetString(string url, string authUser = "", string authPass = "", bool useSSL = false)
+        {
+            if (useSSL)
+                ServicePointManager.SecurityProtocol = this.SecurityProtocolType;
+
+            var handler = new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate | DecompressionMethods.None,
+            };
+
+            if (!String.IsNullOrEmpty(authUser))
+            {
+                var credentialCache = new CredentialCache();
+                credentialCache.Add(new Uri(url), "Basic", new NetworkCredential(authUser, authPass));
+                handler.Credentials = credentialCache;
+            }
+
+            using (HttpClient client = new HttpClient(handler))
+            {
+                client.DefaultRequestHeaders.UserAgent.TryParseAdd(UserAgent);
+                client.DefaultRequestHeaders.Add("accept", this.Accept);
+                client.DefaultRequestHeaders.Add("accept-language", this.AcceptLanguage);
+
+                try
+                {
+                    string html = client.GetStringAsync(url).Result;
+                    client.Dispose();
+
+                    return html;
+                }
+                catch (Exception ex)
+                {
+                    string error = ex.Message;
+                    if (ex.InnerException != null) error += " " + ex.InnerException.Message;
+
+                    return "REQUEST_ERROR_" + error;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get string from a http request, using Proxy
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="proxyIP"></param>
+        /// <param name="proxyPort"></param>
+        /// <param name="proxyUser"></param>
+        /// <param name="proxyPassword"></param>
+        /// <param name="useSSL"></param>
+        /// <returns></returns>
+        public string GetString(string url, string proxyIP, int proxyPort, string proxyUser = "", string proxyPassword = "", bool useSSL = false)
+        {
+            if (useSSL)
+                ServicePointManager.SecurityProtocol = this.SecurityProtocolType;
+
+            
+            WebProxy proxy = new WebProxy(proxyIP, proxyPort);
+            if (String.IsNullOrEmpty(proxyUser))
+            {
+                proxy.UseDefaultCredentials = false;
+                proxy.Credentials = new System.Net.NetworkCredential(proxyUser, proxyPassword);
+            }
+
+            var handler = new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate | DecompressionMethods.None,
+                UseProxy = true,
+                Proxy = proxy
+            };
+
+
+            using (HttpClient client = new HttpClient(handler))
+            {
+                client.DefaultRequestHeaders.UserAgent.TryParseAdd(UserAgent);
+                client.DefaultRequestHeaders.Add("accept", this.Accept);
+                client.DefaultRequestHeaders.Add("accept-language", this.AcceptLanguage);
+
+                try
+                {
+                    string html = client.GetStringAsync(url).Result;
+                    client.Dispose();
+
+                    return html;
+                }
+                catch (Exception ex)
+                {
+                    string error = ex.Message;
+                    if (ex.InnerException != null) error += " " + ex.InnerException.Message;
+
+                    return "REQUEST_ERROR_" + error;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Download file
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="outputFile"></param>
+        /// <param name="msg"></param>
+        /// <param name="useSSL"></param>
+        /// <returns></returns>
+        public bool DownloadFile(string url, string outputFile, out string msg, bool useSSL = false)
+        {
+            if (useSSL)
+                ServicePointManager.SecurityProtocol = this.SecurityProtocolType;
+
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.Accept, "image/jpg");
+                client.Headers.Add(HttpRequestHeader.AcceptLanguage, "en-US,en;q=0.9,vi;q=0.8");
+                client.Headers.Add(HttpRequestHeader.UserAgent, this.UserAgent);
+
+                try
+                {
+                    client.DownloadFile(url, outputFile);
+                    msg = "Success";
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    string error = ex.Message;
+                    if (ex.InnerException != null) error += " " + ex.InnerException.Message;
+                    msg = error;
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ AS.Tools/Logger.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS.Tools
+{
+    /// <summary>
+    /// Log helper
+    /// </summary>
+    public class Logger
+    {
+        private string _logFile;
+        private List<string> _logData { get; set; }
+
+        /// <summary>
+        /// Init with set log file name
+        /// </summary>
+        /// <param name="logFile"></param>
+        public Logger(string logFile)
+        {
+            _logFile = logFile;
+            init();
+        }
+
+        /// <summary>
+        /// Init with default log file name: ddMMyyyy
+        /// </summary>
+        public Logger()
+        {
+            _logFile = $"log_{DateTime.Now.ToString("ddMMyyyy")}";
+            init();
+        }
+
+        /// <summary>
+        /// Apend log to log data
+        /// </summary>
+        /// <param name="text"></param>
+        public void Append(string text)
+        {
+            _logData.Add($"[{DateTime.Now.ToString("hh:mm:ss")}] : {text}");
+        }
+
+        /// <summary>
+        /// Clear all log data
+        /// </summary>
+        public void Clear()
+        {
+            _logData.Clear();
+        }
+
+        /// <summary>
+        /// Save to log\_logFile
+        /// </summary>
+        public void Save()
+        {
+            File.AppendAllLines(AppDomain.CurrentDomain.BaseDirectory + $"\\log\\{_logFile}.txt", _logData);
+            _logData.Clear();
+        }
+
+        private void init()
+        {
+            if (!Directory.Exists("log"))
+                Directory.CreateDirectory("log");
+
+            _logData = new List<string>();
+            _logData.Add($"------------- NEW LOG START AT {DateTime.Now.ToString("hh:mm:ss")} -------------");
+        }
+    }
+}
